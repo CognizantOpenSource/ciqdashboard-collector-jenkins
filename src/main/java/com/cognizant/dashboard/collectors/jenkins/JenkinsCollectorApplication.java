c0b705b9f5d7592e41dc6a943b5ae3d3d335a66d
138497 <subramani.murthy@cognizant.com>
2020-10-27 04:23:03 -0700

    updated

--- src/main/java/com/cognizant/dashboard/collectors/jenkins/JenkinsCollectorApplication.java
+++ src/main/java/com/cognizant/dashboard/collectors/jenkins/JenkinsCollectorApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties
+@EnableAutoConfiguration
+@EnableEncryptableProperties
 public class JenkinsCollectorApplication {
 
 	public static void main(String[] args) {
